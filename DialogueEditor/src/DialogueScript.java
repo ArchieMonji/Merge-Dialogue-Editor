ba36ca180bfcd5716649ce08d1d85010a772dcfc
Archie Monji <amonji@uci.edu>
2013-11-23 17:27:03 -0800

    Initial Commit

---
+++ DialogueEditor/src/DialogueScript.java
@@ -0,0 +1,16 @@
+
+
+public class DialogueScript{
+	public Page[] pages;
+	public String event;
+
+	public static class Page {
+		public String speaker;
+		public Emotion[] emotions;
+	}
+	
+	public static class Emotion{
+		public String image;
+		public String[] lines;
+	}
+}
